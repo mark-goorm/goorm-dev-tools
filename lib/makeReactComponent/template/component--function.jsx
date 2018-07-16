db46a270f0e777e30676b877a4e926c8a43d1f38
minung.han <hmu332233@gmail.com>
2018-07-17 00:57:10 +0900

    [Add] grm cmpnt

---
+++ lib/makeReactComponent/template/component--function.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './[COMPONENT_NAME].[CSS_EXTENTION]';
+
+const [COMPONENT_NAME] = props => {
+  return (
+  );
+}
+
+[COMPONENT_NAME].propTypes = {
+};
+[COMPONENT_NAME].defaultProps = {
+};
+
+export default [COMPONENT_NAME];
---
+++ lib/makeReactComponent/template/component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './[COMPONENT_NAME].[CSS_EXTENTION]';
+
+class [COMPONENT_NAME] extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+    );
+  }
+}
+
+
+[COMPONENT_NAME].propTypes = {
+};
+[COMPONENT_NAME].defaultProps = {
+};
+
+export default [COMPONENT_NAME];
